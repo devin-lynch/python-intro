9dabe743ad6722a44328ee937c534269041ee678
devin-lynch <devin.l.lynch@gmail.com>
2022-10-14 12:30:05 -0500

    Test function

--- intro.py
+++ intro.py
@@ -11,3 +11,15 @@ I am a docstring
 """
 I am also a doc string
 """
+
+# use the built in print function for output
+print('Hello, Spam!')
+
+# ctrl + d leaves the python3 repl
+
+# python functions start with the 'def' keyword
+def greeting(name):
+    # this comment is tabbed in to the function scope
+    print("Hello", name, "how are you liking python?")
+
+# greeting('Gabe')
