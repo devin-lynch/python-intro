52c53cc98fcfb8448f1ef9eb370981e53cac0b33
devin-lynch <devin.l.lynch@gmail.com>
2022-10-14 12:49:18 -0500

    Add conditional

--- intro.py
+++ intro.py
@@ -23,3 +23,56 @@ def greeting(name):
     print("Hello", name, "how are you liking python?")
 
 # greeting('Gabe')
+
+# python_variables_use_snake_case
+# variable names use snake_case
+# function names use snake_case
+
+# define a variable (no variable declaration keyword) (they work as a 'let' -- 'const' does not exist in python)
+my_variable = 'spam'
+print(my_variable)
+my_variable = 'eggs'
+print(my_variable)
+# there is no const, but convention among python programmers indicates consts WITH_CAPS
+MY_CONST = 'do not change me'
+# MY_CONST = 'will this work?'
+
+# ## # ## # ## # ## #
+# DATA TYPES
+
+# Booleans
+None # Nonetype is the absence of explicit value (Nonetype is falsy) -- also used a null
+True # truthy value
+False # falsy value
+
+# booleans and control flow
+has_bank_account = True
+
+if has_bank_account:
+    print('there is a bank account!')
+    print('I am in the if')
+    # 
+    # more code
+    # 
+else:
+    print('there is no bank account!')
+
+# logical operators
+# and -- &&
+# or -- ||
+# not/! -- !
+
+# comparison operators (return bools)
+# == -- equality
+# != -- not equal
+# > < >= <= --  greater than/less than
+
+account_balance = 75
+
+if has_bank_account and account_balance > 99:
+    print('you\'re rich!')
+# else if uses elif keyword
+elif has_bank_account and account_balance > 50:
+    print('keep saving! one day you will be rich!')
+else:
+    print('you are broke!')
